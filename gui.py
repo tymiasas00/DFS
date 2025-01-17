1d12a0edf4fd744b7a82fc9791d3e196e59fbcaa
Tymoteusz Majewski <tymoteusz.majewski1@gmail.com>
2025-01-17 23:06:09 +0100

    Implement GUI for Directed Graph DFS visualization and add sample graph data

--- gui.py
+++ gui.py
@@ -1,9 +1,37 @@
 import tkinter as tk
-from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-class App(tk.Tk):
-    def __init__(self, screenName = None, baseName = None, className = "Tk", useTk = True, sync = False, use = None):
-        super().__init__(screenName, baseName, className, useTk, sync, use)
+class GraphVisualizer:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Directed Graph DFS Visualizer")
 
-        self.title('DFS Visualizer')
-        self.geometry('400x300')
+        # Left Frame
+        self.left_frame = tk.Frame(self.root)
+        self.left_frame.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.Y)
+
+        # Listbox for graphs
+        self.graph_listbox = tk.Listbox(self.left_frame, width=30, height=15)
+        self.graph_listbox.pack(pady=5)
+
+        # Buttons
+        self.load_button = tk.Button(self.left_frame, text="Load Graphs", command=None)
+        self.load_button.pack(pady=5)
+
+        self.visualize_button = tk.Button(self.left_frame, text="Run Visualization", command=None) # TODO : Implement visualization from dfs class
+        self.visualize_button.pack(pady=5)
+
+        # Right Frame
+        self.right_frame = tk.Frame(self.root)
+        self.right_frame.pack(side=tk.RIGHT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+        # Placeholder for graphs
+        self.graphs = {}
+
+     
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = GraphVisualizer(root)
+    root.mainloop()
