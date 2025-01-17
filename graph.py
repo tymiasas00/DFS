12d384b5f26336ee36f38c995413506b3b66ace5
Tymoteusz Majewski <tymoteusz.majewski1@gmail.com>
2025-01-17 22:43:16 +0100

    Enhance DirectedGraph class with type hints and improve GUI initialization for DFS visualization

--- graph.py
+++ graph.py
@@ -3,16 +3,16 @@ import networkx as nx
 class DirectedGraph:
     """Graph representation using adjacency list"""    
 
-    def __init__(self, vertices):
+    def __init__(self, vertices: int):
         """Initializes graph object with given number of vertices"""
         self.vertices = vertices
         self.adjacency = [[] for _ in range(vertices)]
 
-    def add_edge(self, vertex1, vertex2):
+    def add_edge(self, vertex1: int, vertex2: int):
         """Adds edge directed from vertex1 to vertex2"""
         self.adjacency[vertex1].append(vertex2)
 
-    def to_nx_graph(self):
+    def to_nx_graph(self) -> nx.DiGraph:
         """Returns nx.DiGraph object for visualization purpurose"""
         digraph = nx.DiGraph()
         for vertex, neighbors in enumerate(self.adjacency):
